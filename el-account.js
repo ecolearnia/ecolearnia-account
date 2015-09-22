7aca3f42701660c69bcef9193d4d0ca562a6c338
Young Suk Ahn <ys.ahnpark@gmail.com>
2015-09-21 22:46:21 -0400

    Move bell registration to lib/index.js Add model/auth.js and model/account.js Add HapiResource for account

--- el-account.js
+++ el-account.js
@@ -4,10 +4,15 @@ var config = require('ecofyjs-config');
 // Authentication for social networks
 var Bell = require('bell');
 
+// Mongo Database connection
+var DbUtils = require('./lib/utils/mongoutils').MongoUtils;
+
 config.load('./config/el-account.conf.json');
 var port = config.get('port');
 var logConf = config.get('log');
 
+// Should this go in the application file?
+DbUtils.connect('mongodb://localhost/ecol_test');
 
 var server = new Hapi.Server();
 server.connection(
@@ -27,52 +32,10 @@ server.register(require('vision'), function () {});
 server.register(require('inert'), function () {});
 
 server.register([
-      // Registering the 3rd party authentication plugin (for G+)
-      { register: Bell },
       // Registering the account plugin
       { register: require("./lib/index"), options: { log: logConf} }
 ], function(err) {
     if (err) throw err;
-
-    var googleClientSecret = process.env.GOOGLE_CLIENT_SECRET; 
-    console.log('Google googleClientSecret=' + googleClientSecret);
-  // Registering Bell strategy with Google+ setting
-  server.auth.strategy('google', 'bell', {
-        provider: 'google',
-        password: 'password',
-        isSecure: false,
-        // Go to https://console.developers.google.com
-        // Create a new project.
-        // Navigate to “APIs & auth /  APIs” and filter by “Google+ API”
-        // Click the “Google+ API” and press the “Enable API” button on top
-        // Navigate to “APIs & auth /  Credentials”
-        // Press “Add credentials” and in the drop down select “OAuth 2.0 client ID”
-        // Select “Web application”
-        // Enter the name of the application
-        // In the field “Authorized JavaScript origins” enter the “http://localhost:<port>”
-        // In the field “Authorized redirect URIs” enter “http://localhost:<port>/bell/door“
-        clientId: '800325046946-grh3dm8pik124qcusb5j6ah6ok3tchei.apps.googleusercontent.com',
-        clientSecret: googleClientSecret,
-        location: server.info.uri
-    });
-
-    server.route({
-        method: '*',
-        path: '/bell/door',
-        config: {
-            auth: {
-                strategy: 'google',
-                mode: 'try'
-            },
-            handler: function (request, reply) {
-
-                if (!request.auth.isAuthenticated) {
-                    return reply('Authentication failed due to: ' + request.auth.error.message);
-                }
-                reply('<pre>' + JSON.stringify(request.auth.credentials, null, 4) + '</pre>');
-            }
-        }
-    });
 
     server.start(function() {
         console.log("EcoLearnia-Account server started @ " + server.info.uri);
